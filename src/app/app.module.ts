094450ae435bc479fed4f747131877f2fc202bea
M Gosschalk <monica.gosschalk@btinternet.com>
2017-11-10 17:36:00 +0000

    added login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+// feature modules
+import { AuthModule } from './auth/auth.module';
+
+// routes
+export const ROUTES: Routes = [];
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/*
+var config = {
+    apiKey: "AIzaSyCwsjPTUUKj-6Mt9vC656MJKOk5AY_81Qs",
+    authDomain: "angular2-course-a8e4b.firebaseapp.com",
+    databaseURL: "https://angular2-course-a8e4b.firebaseio.com",
+    projectId: "angular2-course-a8e4b",
+    storageBucket: "angular2-course-a8e4b.appspot.com",
+    messagingSenderId: "829202919005"
+  };
+  */
