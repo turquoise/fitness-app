04129586beff2abb86119c5aa510d85244a5ef91
M Gosschalk <monica.gosschalk@btinternet.com>
2017-11-14 14:55:37 +0000

    added schedule assignment

--- src/app/health/schedule/containers/schedule/schedule.component.ts
+++ src/app/health/schedule/containers/schedule/schedule.component.ts
@@ -3,6 +3,8 @@ import { Store } from '../../../../../store';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { ScheduleService, ScheduleItem } from '../../../shared/services/schedule/schedule.service';
+import { Meal, MealsService } from '../../../shared/services/meals/meals.service';
+import { Workout, WorkoutsService } from '../../../shared/services/workouts/workouts.service';
 
 @Component({
   selector: 'schedule',
@@ -16,18 +18,30 @@ import { ScheduleService, ScheduleItem } from '../../../shared/services/schedule
         (select)="changeSection($event)">
       </schedule-calendar>
 
+      <schedule-assign
+        *ngIf="open"
+        [section]="selected$ | async"
+        [list]="list$ | async">
+      </schedule-assign>
+
     </div>
   `
 })
 
 export class ScheduleComponent implements OnInit, OnDestroy {
 
+  open = false;
+
   date$: Observable<Date>;
+  selected$: Observable<any>;
+  list$: Observable<Meal[] | Workout[]>;
   schedule$: Observable<ScheduleItem[]>;
   subscriptions: Subscription[] = [];
 
   constructor(
     private store: Store,
+    private mealsService: MealsService,
+    private workoutsService: WorkoutsService,
     private scheduleService: ScheduleService
   ) {}
 
@@ -37,6 +51,7 @@ export class ScheduleComponent implements OnInit, OnDestroy {
   }
 
   changeSection(event: any) {
+    this.open = true;
     //console.log(event);
     this.scheduleService.selectSection(event);
   }
@@ -44,9 +59,15 @@ export class ScheduleComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.date$ = this.store.select('date');
     this.schedule$ = this.store.select('schedule');
+    this.selected$ = this.store.select('selected');
+    this.list$ = this.store.select('list');
+
     this.subscriptions = [
       this.scheduleService.schedule$.subscribe(),
-      this.scheduleService.selected$.subscribe()
+      this.scheduleService.selected$.subscribe(),
+      this.scheduleService.list$.subscribe(),
+      this.mealsService.meals$.subscribe(),
+      this.workoutsService.workouts$.subscribe()
     ];
   }
 
